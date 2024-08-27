5036db891b0dbf32d014bd4f4f1c9018c840e677
ifahad50 <ifahad50@gmail.com>
2024-08-27 12:33:51 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
+import NextTopLoader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,17 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <NextTopLoader
+          color='#212020'
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing='ease'
+          speed={200}
+          shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+        />
         {children}
         <Toaster />
       </body>
